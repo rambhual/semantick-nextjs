5b22784c7f663b1ae0058fa3b8c27619b9811c0c
unknown <Rampritsahani@outlook.in>
2019-09-27 08:34:35 +0530

    adding active route link

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 import { Menu, Button } from "semantic-ui-react";
 import Link from "next/link";
 
 function Header() {
+  const router = useRouter();
+  const isActive = route => {
+    return route == router.pathname;
+  };
+
   const [user, setUser] = React.useState(false);
   return (
     <>
@@ -15,7 +21,11 @@ function Header() {
         style={{ borderRadius: 0, minHeight: "4em" }}
       >
         <Link href="/">
-          <Menu.Item style={{ fontSize: "1.4em" }} header>
+          <Menu.Item
+            style={{ fontSize: "1.4em" }}
+            header
+            active={isActive("/")}
+          >
             Dhanai Fruits Mart
           </Menu.Item>
         </Link>
@@ -24,13 +34,13 @@ function Header() {
           {user ? (
             <>
               <Link href="/cart">
-                <Menu.Item name="Cart" />
+                <Menu.Item name="Cart" active={isActive("/cart")} />
               </Link>
               <Link href="/create">
-                <Menu.Item name="Create" />
+                <Menu.Item name="Create" active={isActive("/create")} />
               </Link>
               <Link href="/account">
-                <Menu.Item name="Account" />
+                <Menu.Item name="Account" active={isActive("/account")} />
               </Link>
 
               <Menu.Item>
@@ -42,10 +52,10 @@ function Header() {
           ) : (
             <>
               <Link href="/login">
-                <Menu.Item name="Login" />
+                <Menu.Item name="Login" active={isActive("/login")} />
               </Link>
               <Link href="/signup">
-                <Menu.Item name="Sign up" />
+                <Menu.Item name="Sign up" active={isActive("/signup")} />
               </Link>
             </>
           )}
